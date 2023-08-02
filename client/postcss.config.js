a46dd7e4634c66c3b9fc5ffc4353d977b85f76b7
albe68 <albertpaulus836@gmail.com>
2023-08-02 10:43:12 +0530

    fixed css bug not loaded css in browser while after configuring the postcss used es6 import/export

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,7 +1,10 @@
+// postcss.config.js
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
-export const plugins=[
- tailwindcss,
- autoprefixer
-]
+export default {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ]
+};
