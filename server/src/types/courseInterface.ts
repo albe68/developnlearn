648b3f41f20e3f1f966bbdc09e60d5a860e95ccb
albe68 <albertpaulus836@gmail.com>
2023-08-08 15:54:03 +0530

    stage

---
+++ server/src/types/courseInterface.ts
@@ -0,0 +1,9 @@
+export interface  AddCourseDetailsInterface{
+    title:string,
+    duration:number,
+    level:string,
+    tags:string[]|string,
+    description:string,
+    requirements:string,
+    price:number
+}
