1616603ac4482d60afdc2eb953187a92e6f5b224
albe68 <albertpaulus836@gmail.com>
2023-08-25 23:03:19 +0530

    secret introduced to server

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 const configKeys={
     
+
     MONGO_DB_URL: process.env.DATABASE as string,
 
     PORT: process.env.PORT,
@@ -10,6 +11,8 @@ const configKeys={
     DB_NAME : process.env.DB_NAME as string,
 
     JWT_SECRET : process.env.JWT_SECRET as string,
+
+    REFRESH_JWT_SECRET : process.env.REFRESH_JWT_SECRET as string,
 
     NODE_ENV :process.env.NODE_ENV as string,
 
@@ -24,6 +27,16 @@ const configKeys={
     CLOUD_KEY:process.env.API_KEY as string,
 
     CLOUD_SEC:process.env.API_SECRET as string,
+
+    PAYMENT_SECRET:process.env.STRIPE_SECRET_KEY as string,
+
+    PAYMENT_PUBLISH_KEY:process.env.STRIPE_PUBLISH_KEY as string,
+   
+    RAZOR_PAY_ID: process.env.RAZOR_PAY_ID as string,
+
+    RAZOR_PAY_SECRET_KEY:process.env.RAZOR_SECRET_KEY as string
+
+
 
 }
 export default configKeys;
